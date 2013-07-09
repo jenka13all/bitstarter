28c24e7dfee6e44733f0a755cedc98ce5ff72313
jenka13all <jenka13all@gmail.com>
2013-07-09 22:50:07 +0200

    returns stringified buffer of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
+
+var hello_buffer = fs.readFileSnync("index.html");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(hello_buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
